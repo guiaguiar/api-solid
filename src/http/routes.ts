55b1f085fbf5bba3e9df65602b7674378c007b8f
Guilherme Aguiar <gui-aguiar9@hotmail.com>
2024-03-31 01:45:16 -0300

    returning user profile on /me route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,13 @@
 import { profile } from '@/http/controllers/profile'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 	app.post('/sessions', authenticate)
 
 	// Authenticated
-	app.get('/me', profile)
+	app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
