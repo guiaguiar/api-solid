6c3a2303872a8a55076d341ec378ebf36dffe330
Guilherme Aguiar <gui-aguiar9@hotmail.com>
2024-03-30 23:45:37 -0300

    validating check in date

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,24 +1,28 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-	async findByEmail(email: string) {
-		const user = await prisma.user.findUnique({
-			where: {
-				email,
-			},
-		})
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
 
-		return user
-	}
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
 
-	async create(data: Prisma.UserCreateInput) {
-		const user = await prisma.user.create({
-			data,
-		})
+    return user
+  }
 
-		return user
-	}
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
 }
