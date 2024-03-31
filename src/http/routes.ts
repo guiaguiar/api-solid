d653461d2d2f52286f3e5a51311c789f069116d7
Guilherme Aguiar <gui-aguiar9@hotmail.com>
2024-03-31 01:40:38 -0300

    creating jwt auth

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
+import { profile } from '@/http/controllers/profile'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
-
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 	app.post('/sessions', authenticate)
+
+	// Authenticated
+	app.get('/me', profile)
 }
