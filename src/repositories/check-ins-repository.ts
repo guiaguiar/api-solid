382094da1d4f5d3449be9e7c6f55719080d67392
Guilherme Aguiar <gui-aguiar9@hotmail.com>
2024-03-31 00:45:12 -0300

    created factories from use cases

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,8 +1,11 @@
 import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
+  findById(id: string): Promise<CheckIn | null>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
+  save(checkIn: CheckIn): Promise<CheckIn>
+
 }
